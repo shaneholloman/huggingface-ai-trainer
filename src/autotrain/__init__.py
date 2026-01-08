bc7ef0dc37a5bc9e994ee596fd922b45ff97e7ce
RunPod Training <runpod@training.local>
2026-01-07 23:19:26 -0300

    Bump version to 0.0.17

--- src/autotrain/__init__.py
+++ src/autotrain/__init__.py
@@ -45,7 +45,7 @@ warnings.filterwarnings("ignore", category=FutureWarning, module="accelerate")
 warnings.filterwarnings("ignore", category=UserWarning, module="huggingface_hub")
 
 logger = Logger().get_logger()
-__version__ = "0.0.16"
+__version__ = "0.0.17"
 
 
 def is_colab():
